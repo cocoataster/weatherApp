353f1a6d4374ef7c962be6ea1311ee0f29b651c8
Eric Sans Alvarez <ericsansalvarez@Erics-MacBook-Pro.local>
2017-05-23 13:36:54 +0200

    Constants + CurrentWeather class

---
+++ weatherApp/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  weatherApp
+//
+//  Created by Eric Sans Alvarez on 23/05/2017.
+//  Copyright © 2017 Eric Sans Alvarez. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+
+let LATITUDE = "lat="
+
+let LONGITUDE = "&lon"
+
+let APP_ID = "&appid="
+
+let API_KEY = "beb0f35ba4b137fcb10c85a8b3218aae"
+
+let CURRENT_WEATHER_URL = BASE_URL + LATITUDE + LONGITUDE + APP_ID + API_KEY
